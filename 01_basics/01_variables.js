7f2db929f2dc60a68f111fac1fe563cb03c83956
Avirup Das <40535294+Avirupd@users.noreply.github.com>
2024-02-22 18:07:19 +0000

    Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 123456
+let accountEmail = "avirupdas@yahoo.com"
+let accountPassword ="123456"
+accountCity = "Kolkata"
+let accountState;
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Don't use var because of the block scope or funtional scope
+
+*/
