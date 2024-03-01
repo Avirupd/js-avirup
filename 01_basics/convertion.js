df0ff1b3d438b90ab4f1c05ee43a4c612480f6e2
Avirup Das <40535294+Avirupd@users.noreply.github.com>
2024-03-01 16:32:01 +0000

    Convertion Basics is Done

--- 01_basics/convertion.js
+++ 01_basics/convertion.js
@@ -1,11 +1,11 @@
 let age = "26"
 
-console.log(typeof age)
-console.log(typeof (age))
+// console.log(typeof age)
+// console.log(typeof (age))
 
 let valueInNumber = Number(age)
-console.log(typeof age)
-console.log(age)
+// console.log(typeof age)
+// console.log(age)
 
 
 //33 => 33
@@ -13,3 +13,28 @@ console.log(age)
 //"33abc" => NaN (Not a Number)
 //true => 1, false=>0
 //abc => NaN (Not a Number)
+// null => 0
+// undefined => NaN (Not a Number)
+
+
+let isLoggedIn = "Avirup"
+
+let bool = Boolean(isLoggedIn)
+
+// console.log(typeof bool)
+// console.log(bool)
+
+//"1" => true
+// "" => false
+// "Avirup" => true
+// "0" => false
+
+let num = 10
+
+let strNum = String(num)
+
+console.log(typeof strNum)
+console.log(strNum)
+
+// 10 => 10
+
