a5b5256b5ffe2f28d628f4e998e13f05b3afd4b0
Avirup Das <40535294+Avirupd@users.noreply.github.com>
2024-03-01 16:13:33 +0000

    Done

---
+++ 01_basics/convertion.js
@@ -0,0 +1,15 @@
+let age = "26"
+
+console.log(typeof age)
+console.log(typeof (age))
+
+let valueInNumber = Number(age)
+console.log(typeof age)
+console.log(age)
+
+
+//33 => 33
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+//true => 1, false=>0
+//abc => NaN (Not a Number)
